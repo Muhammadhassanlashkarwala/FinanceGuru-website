f66a4ca03f5fdca92dfa6dcec17ed05a9390ef8f
Muhammadhassanlashkarwala <bhaihassan036@gmail.com>
2024-09-12 03:58:28 +0500

    push

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,41 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import headerImage from './assets/header.png'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header className="text-gray-600 body-font">
+    <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+      <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+      <img src={headerImage} alt="" className='w-10 h-10' />
+        <span className="ml-3 text-xl cursor-pointer">Landing-page</span>
+      </a>
+      <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
+        <a className="mr-5 hover:text-gray-900 cursor-pointer">Home</a>
+        <a className="mr-5 hover:text-gray-900 cursor-pointer">Service</a>
+        <a className="mr-5 hover:text-gray-900 cursor-pointer">About</a>
+        <a className="mr-5 hover:text-gray-900 cursor-pointer">Contact</a>
+      </nav>
+      <button className="inline-flex items-center text-gray-900 bg-white border border-black py-1 px-3 focus:outline-none hover:bg-blue-400 border-none rounded text-base mt-4 md:mt-0">
+        Button
+        <svg
+          fill="none"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          className="w-4 h-4 ml-1"
+          viewBox="0 0 24 24"
+        >
+          <path d="M5 12h14M12 5l7 7-7 7" />
+        </svg>
+      </button>
+    </div>
+  </header>
     </>
   )
 }
