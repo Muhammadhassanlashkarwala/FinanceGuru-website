a27d7037a29898c25e30b4d1b4d84f2d760d5f59
Muhammadhassanlashkarwala <bhaihassan036@gmail.com>
2024-09-13 04:00:20 +0500

    components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import headerImage from './assets/header.png'
+
+import Header from './assets/Components.jsx/Header.jsx'
+import Hero from './assets/Components.jsx/Hero.jsx'
+import Feature from './assets/Components.jsx/Features.jsx'
+import Testimonial from './assets/Components.jsx/Testimonial.jsx'
+import Footer from './assets/Components.jsx/Footer.jsx'
 import financeImage from './assets/finance-guru.png'
+
 
 function App() {
 
@@ -11,37 +13,11 @@ function App() {
   return (
     <>
 {/* Header Section */}
-
-      <header className="text-gray-600 body-font">
-    <div className="container mx-auto flex flex-wrap p-5 px-12 flex-col md:flex-row items-center">
-      <a className="flex title-font px-12 font-medium items-center text-gray-900 mb-4 md:mb-0">
-      <img src={headerImage} alt="logo-image" className='w-8 h-8 ' />
-        <span className="ml-3 text-xl cursor-pointer">FinanceGuru</span>
-      </a>
-      <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-        <a className="mr-5 hover:text-gray-900 cursor-pointer">Home</a>
-        <a className="mr-5 hover:text-gray-900 cursor-pointer">Service</a>
-        <a className="mr-5 hover:text-gray-900 cursor-pointer">About</a>
-        <a className="mr-5 hover:text-gray-900 cursor-pointer">Contact</a>
-      </nav>
-      <button className="inline-flex items-center text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded text-base mt-4 md:mt-0">
-        Button
-        <svg
-          fill="none"
-          stroke="currentColor"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth={2}
-          className="w-4 h-4 ml-1"
-          viewBox="0 0 24 24"
-        >
-          <path d="M5 12h14M12 5l7 7-7 7" />
-        </svg>
-      </button>
-    </div>
-  </header>
+  <Header />
+     
 
   {/* Hero Section */}
+  
   <section className="text-gray-600 body-font">
     <div className="container mx-auto flex px-10 py-24 md:flex-row flex-col items-center">
       <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
@@ -73,304 +49,19 @@ function App() {
   </section>
  
   {/* Features Section */}
-  <section className="text-gray-600 body-font">
-    <div className="container px-5 py-24 mx-auto">
-      <h1 className="sm:text-3xl text-2xl font-medium title-font text-center text-gray-900 mb-20">
-       Finance Guru Features
-      </h1>
-      <div className="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6 pl-20">
-        <div className="p-4 md:w-1/3 flex">
-          <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-blue-500 mb-4 flex-shrink-0">
-            <svg
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              className="w-6 h-6"
-              viewBox="0 0 24 24"
-            >
-              <path d="M22 12h-4l-3 9L9 3l-3 9H2" />
-            </svg>
-          </div>
-          <div className="flex-grow pl-6">
-            <h2 className="text-gray-900 text-lg title-font font-medium mb-2">
-              Budgeting
-            </h2>
-            <p className="leading-relaxed text-base">
-             Set monthly budgets and stick to them 
-             <br className="hidden lg:inline-block" />
-             to save more.
-            </p>
-            <a className="mt-3 text-blue-500 inline-flex items-center">
-              Learn More
-              <svg
-                fill="none"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                className="w-4 h-4 ml-2"
-                viewBox="0 0 24 24"
-              >
-                <path d="M5 12h14M12 5l7 7-7 7" />
-              </svg>
-            </a>
-          </div>
-        </div>
-        <div className="p-4 md:w-1/3 flex">
-          <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-blue-500 mb-4 flex-shrink-0">
-            <svg
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              className="w-6 h-6"
-              viewBox="0 0 24 24"
-            >
-              <circle cx={6} cy={6} r={3} />
-              <circle cx={6} cy={18} r={3} />
-              <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12" />
-            </svg>
-          </div>
-          <div className="flex-grow pl-6">
-            <h2 className="text-gray-900 text-lg title-font font-medium mb-2">
-              Expense Tracking
-            </h2>
-            <p className="leading-relaxed text-base">
-            Easily track yoyur daily expenses and see
-            <br className="hidden lg:inline-block" />
-             where your money goes.
-            </p>
-            <a className="mt-3 text-blue-500 inline-flex items-center">
-              Learn More
-              <svg
-                fill="none"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                className="w-4 h-4 ml-2"
-                viewBox="0 0 24 24"
-              >
-                <path d="M5 12h14M12 5l7 7-7 7" />
-              </svg>
-            </a>
-          </div>
-        </div>
-        <div className="p-4 md:w-1/3 flex">
-          <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-blue-500 mb-4 flex-shrink-0">
-            <svg
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              className="w-6 h-6"
-              viewBox="0 0 24 24"
-            >
-              <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2" />
-              <circle cx={12} cy={7} r={4} />
-            </svg>
-          </div>
-          <div className="flex-grow pl-6">
-            <h2 className="text-gray-900 text-lg title-font font-medium mb-2">
-              Financial Goals 
-            </h2>
-            <p className="leading-relaxed text-base">
-              Set and achieve your financial goals with
-              <br className="hidden lg:inline-block" />
-               personalized tips and insights.
-            </p>
-            <a className="mt-3 text-blue-500 inline-flex items-center">
-              Learn More
-              <svg
-                fill="none"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                className="w-4 h-4 ml-2"
-                viewBox="0 0 24 24"
-              >
-                <path d="M5 12h14M12 5l7 7-7 7" />
-              </svg>
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
-
+     <Feature />
 
 {/* Testimonial Section */}
+   <Testimonial />
 
-<section className="text-gray-600 body-font ">
-  <div className="container px-20 py-24 mx-auto">
-    <h1 className="text-3xl font-medium title-font text-gray-900 mb-12 text-center">
-      Testimonials
-    </h1>
-    <div className="flex flex-wrap -m-4">
-      <div className="p-4 md:w-1/2 w-full">
-        <div className="h-full bg-gray-100 p-8 rounded">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            className="block w-5 h-5 text-gray-400 mb-4"
-            viewBox="0 0 975.036 975.036"
-          >
-            <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z" />
-          </svg>
-          <p className="leading-relaxed mb-6">
-          FinanceGuru has completely transformed the way I manage my 
-          <br className="hidden lg:inline-block" />
-          money. Highly recommended!
-          </p>
-          <a className="inline-flex items-center">
-            <img
-              alt="testimonial"
-              src="https://dummyimage.com/106x106"
-              className="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center"
-            />
-            <span className="flex-grow flex flex-col pl-4">
-              <span className="title-font font-medium text-gray-900 ">
-             Alice Jhonson
-              </span>
-              <span className="text-gray-500 text-sm">MANAGER</span>
-            </span>
-          </a>
-        </div>
-      </div>
-      <div className="p-4 md:w-1/2 w-full">
-        <div className="h-full bg-gray-100 p-8 rounded">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            className="block w-5 h-5 text-gray-400 mb-4"
-            viewBox="0 0 975.036 975.036"
-          >
-            <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z" />
-          </svg>
-          <p className="leading-relaxed mb-6">
-           The budgeting feature is fantastic I've savedso much more since
-           <br className="hidden lg:inline-block" />
-            using this app
-          </p>
-          <a className="inline-flex items-center">
-            <img
-              alt="testimonial"
-              src="https://dummyimage.com/107x107"
-              className="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center"
-            />
-            <span className="flex-grow flex flex-col pl-4">
-              <span className="title-font font-medium text-gray-900">
-             Bob Lee
-              </span>
-              <span className="text-gray-500 text-sm">USER</span>
-            </span>
-          </a>
-        </div>
-      </div>
-    </div>
-  </div>
-  <hr />
-</section>
 
 
 {/* Footer Section */}
-
-<footer className="text-gray-600 body-font">
-  <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-    <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth={2}
-        className="w-10 h-10 text-white p-2 bg-blue-500 rounded-full"
-        viewBox="0 0 24 24"
-      >
-        <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5" />
-      </svg>
-      <span className="ml-3 text-xl">FinanceGuru</span>
-    </a>
-    <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
-      © 2020 FinanceGuru —
-      <a
-        href="https://twitter.com/knyttneve"
-        className="text-gray-600 ml-1"
-        rel="noopener noreferrer"
-        target="_blank"
-      >
-        @muhammadhassan
-      </a>
-    </p>
-    <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-      <a className="text-gray-500">
-        <svg
-          fill="currentColor"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth={2}
-          className="w-5 h-5"
-          viewBox="0 0 24 24"
-        >
-          <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z" />
-        </svg>
-      </a>
-      <a className="ml-3 text-gray-500">
-        <svg
-          fill="currentColor"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth={2}
-          className="w-5 h-5"
-          viewBox="0 0 24 24"
-        >
-          <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z" />
-        </svg>
-      </a>
-      <a className="ml-3 text-gray-500">
-        <svg
-          fill="none"
-          stroke="currentColor"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth={2}
-          className="w-5 h-5"
-          viewBox="0 0 24 24"
-        >
-          <rect width={20} height={20} x={2} y={2} rx={5} ry={5} />
-          <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01" />
-        </svg>
-      </a>
-      <a className="ml-3 text-gray-500">
-        <svg
-          fill="currentColor"
-          stroke="currentColor"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth={0}
-          className="w-5 h-5"
-          viewBox="0 0 24 24"
-        >
-          <path
-            stroke="none"
-            d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
-          />
-          <circle cx={4} cy={4} r={2} stroke="none" />
-        </svg>
-      </a>
-    </span>
-  </div>
-</footer>
-
+     
+<Footer />
 
     </>
   )
 }
 
-export default App
+export default App;
---
+++ src/assets/Components.jsx/Features.jsx
@@ -0,0 +1,141 @@
+
+
+function Feature() {
+    
+return(
+
+    <section className="text-gray-600 body-font">
+    <div className="container px-5 py-24 mx-auto">
+      <h1 className="sm:text-3xl text-2xl font-medium title-font text-center text-gray-900 mb-20">
+       Finance Guru Features
+      </h1>
+      <div className="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6 pl-20">
+        <div className="p-4 md:w-1/3 flex">
+          <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-blue-500 mb-4 flex-shrink-0">
+            <svg
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              className="w-6 h-6"
+              viewBox="0 0 24 24"
+            >
+              <path d="M22 12h-4l-3 9L9 3l-3 9H2" />
+            </svg>
+          </div>
+          <div className="flex-grow pl-6">
+            <h2 className="text-gray-900 text-lg title-font font-medium mb-2">
+              Budgeting
+            </h2>
+            <p className="leading-relaxed text-base">
+             Set monthly budgets and stick to them 
+             <br className="hidden lg:inline-block" />
+             to save more.
+            </p>
+            <a className="mt-3 text-blue-500 inline-flex items-center">
+              Learn More
+              <svg
+                fill="none"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                className="w-4 h-4 ml-2"
+                viewBox="0 0 24 24"
+              >
+                <path d="M5 12h14M12 5l7 7-7 7" />
+              </svg>
+            </a>
+          </div>
+        </div>
+        <div className="p-4 md:w-1/3 flex">
+          <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-blue-500 mb-4 flex-shrink-0">
+            <svg
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              className="w-6 h-6"
+              viewBox="0 0 24 24"
+            >
+              <circle cx={6} cy={6} r={3} />
+              <circle cx={6} cy={18} r={3} />
+              <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12" />
+            </svg>
+          </div>
+          <div className="flex-grow pl-6">
+            <h2 className="text-gray-900 text-lg title-font font-medium mb-2">
+              Expense Tracking
+            </h2>
+            <p className="leading-relaxed text-base">
+            Easily track yoyur daily expenses and see
+            <br className="hidden lg:inline-block" />
+             where your money goes.
+            </p>
+            <a className="mt-3 text-blue-500 inline-flex items-center">
+              Learn More
+              <svg
+                fill="none"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                className="w-4 h-4 ml-2"
+                viewBox="0 0 24 24"
+              >
+                <path d="M5 12h14M12 5l7 7-7 7" />
+              </svg>
+            </a>
+          </div>
+        </div>
+        <div className="p-4 md:w-1/3 flex">
+          <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-blue-500 mb-4 flex-shrink-0">
+            <svg
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              className="w-6 h-6"
+              viewBox="0 0 24 24"
+            >
+              <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2" />
+              <circle cx={12} cy={7} r={4} />
+            </svg>
+          </div>
+          <div className="flex-grow pl-6">
+            <h2 className="text-gray-900 text-lg title-font font-medium mb-2">
+              Financial Goals 
+            </h2>
+            <p className="leading-relaxed text-base">
+              Set and achieve your financial goals with
+              <br className="hidden lg:inline-block" />
+               personalized tips and insights.
+            </p>
+            <a className="mt-3 text-blue-500 inline-flex items-center">
+              Learn More
+              <svg
+                fill="none"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                className="w-4 h-4 ml-2"
+                viewBox="0 0 24 24"
+              >
+                <path d="M5 12h14M12 5l7 7-7 7" />
+              </svg>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+);
+
+}
+
+export default Feature;
