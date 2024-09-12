d9e211f7e3bd7b1b4bfdb7f522bc9866cd598645
Muhammadhassanlashkarwala <bhaihassan036@gmail.com>
2024-09-12 14:58:54 +0500

    push

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,19 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import headerImage from './assets/header.png'
+import financeImage from './assets/finance-guru.png'
 
 function App() {
 
 
   return (
     <>
+{/* Header Section */}
+
       <header className="text-gray-600 body-font">
-    <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-      <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-      <img src={headerImage} alt="" className='w-10 h-10' />
+    <div className="container mx-auto flex flex-wrap p-5 px-12 flex-col md:flex-row items-center">
+      <a className="flex title-font px-12 font-medium items-center text-gray-900 mb-4 md:mb-0">
+      <img src={headerImage} alt="" className='w-10 h-10 ' />
         <span className="ml-3 text-xl cursor-pointer">Landing-page</span>
       </a>
       <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
@@ -21,7 +24,7 @@ function App() {
         <a className="mr-5 hover:text-gray-900 cursor-pointer">About</a>
         <a className="mr-5 hover:text-gray-900 cursor-pointer">Contact</a>
       </nav>
-      <button className="inline-flex items-center text-gray-900 bg-white border border-black py-1 px-3 focus:outline-none hover:bg-blue-400 border-none rounded text-base mt-4 md:mt-0">
+      <button className="inline-flex items-center text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded text-base mt-4 md:mt-0">
         Button
         <svg
           fill="none"
@@ -37,6 +40,176 @@ function App() {
       </button>
     </div>
   </header>
+
+  {/* Hero Section */}
+  <section className="text-gray-600 body-font">
+    <div className="container mx-auto flex px-10 py-24 md:flex-row flex-col items-center">
+      <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+        <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+          Take Control of Your Finances
+          <br className="hidden lg:inline-block" />
+     Today
+        </h1>
+        <p className="mb-8 leading-relaxed">
+         Track expenses, set budgets, and achieve your financial goals with
+         <br className="hidden lg:inline-block" />
+          FinanceGuru. 
+        </p>
+        <div className="flex justify-center">
+          <button className="inline-flex text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded text-lg">
+            Get started now
+          </button>
+       
+        </div>
+      </div>
+      <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
+        <img
+          className="object-cover object-center rounded border border-blue-800"
+          alt="hero"
+          src={financeImage}
+        />
+      </div>
+    </div>
+  </section>
+ 
+  {/* Features Section */}
+  <section className="text-gray-600 body-font">
+    <div className="container px-5 py-24 mx-auto">
+      <h1 className="sm:text-3xl text-2xl font-medium title-font text-center text-gray-900 mb-20">
+        Raw Denim Heirloom Man Braid
+        <br className="hidden sm:block" />
+        Selfies Wayfarers
+      </h1>
+      <div className="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6">
+        <div className="p-4 md:w-1/3 flex">
+          <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4 flex-shrink-0">
+            <svg
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              className="w-6 h-6"
+              viewBox="0 0 24 24"
+            >
+              <path d="M22 12h-4l-3 9L9 3l-3 9H2" />
+            </svg>
+          </div>
+          <div className="flex-grow pl-6">
+            <h2 className="text-gray-900 text-lg title-font font-medium mb-2">
+              Shooting Stars
+            </h2>
+            <p className="leading-relaxed text-base">
+              Blue bottle crucifix vinyl post-ironic four dollar toast vegan
+              taxidermy. Gastropub indxgo juice poutine, ramps microdosing banh
+              mi pug VHS try-hard ugh iceland kickstarter tumblr live-edge
+              tilde.
+            </p>
+            <a className="mt-3 text-indigo-500 inline-flex items-center">
+              Learn More
+              <svg
+                fill="none"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                className="w-4 h-4 ml-2"
+                viewBox="0 0 24 24"
+              >
+                <path d="M5 12h14M12 5l7 7-7 7" />
+              </svg>
+            </a>
+          </div>
+        </div>
+        <div className="p-4 md:w-1/3 flex">
+          <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4 flex-shrink-0">
+            <svg
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              className="w-6 h-6"
+              viewBox="0 0 24 24"
+            >
+              <circle cx={6} cy={6} r={3} />
+              <circle cx={6} cy={18} r={3} />
+              <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12" />
+            </svg>
+          </div>
+          <div className="flex-grow pl-6">
+            <h2 className="text-gray-900 text-lg title-font font-medium mb-2">
+              The Catalyzer
+            </h2>
+            <p className="leading-relaxed text-base">
+              Blue bottle crucifix vinyl post-ironic four dollar toast vegan
+              taxidermy. Gastropub indxgo juice poutine, ramps microdosing banh
+              mi pug VHS try-hard ugh iceland kickstarter tumblr live-edge
+              tilde.
+            </p>
+            <a className="mt-3 text-indigo-500 inline-flex items-center">
+              Learn More
+              <svg
+                fill="none"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                className="w-4 h-4 ml-2"
+                viewBox="0 0 24 24"
+              >
+                <path d="M5 12h14M12 5l7 7-7 7" />
+              </svg>
+            </a>
+          </div>
+        </div>
+        <div className="p-4 md:w-1/3 flex">
+          <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4 flex-shrink-0">
+            <svg
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              className="w-6 h-6"
+              viewBox="0 0 24 24"
+            >
+              <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2" />
+              <circle cx={12} cy={7} r={4} />
+            </svg>
+          </div>
+          <div className="flex-grow pl-6">
+            <h2 className="text-gray-900 text-lg title-font font-medium mb-2">
+              Neptune
+            </h2>
+            <p className="leading-relaxed text-base">
+              Blue bottle crucifix vinyl post-ironic four dollar toast vegan
+              taxidermy. Gastropub indxgo juice poutine, ramps microdosing banh
+              mi pug VHS try-hard ugh iceland kickstarter tumblr live-edge
+              tilde.
+            </p>
+            <a className="mt-3 text-indigo-500 inline-flex items-center">
+              Learn More
+              <svg
+                fill="none"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                className="w-4 h-4 ml-2"
+                viewBox="0 0 24 24"
+              >
+                <path d="M5 12h14M12 5l7 7-7 7" />
+              </svg>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+
     </>
   )
 }
